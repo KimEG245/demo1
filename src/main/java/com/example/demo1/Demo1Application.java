59c245f3514d43fbfc4f4a4acbf96e7434f75ce6
KimEG245 <kug7412@gmail.com>
2026-04-24 14:16:19 +0900

    C 클래스 D로 변경 완료

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -28,7 +28,7 @@ public class Demo1Application {
     public String getA() {
         return "A";
     }
-    public String getC() {
-        return "C";
+    public String getD() {
+        return "D";
     }
 }
